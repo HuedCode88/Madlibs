3521ade6ff0168675f2ad5afcc0d8a37d0061238
bossi <bossi@GOOBIS>
2022-10-03 09:43:54 -0400

    Add project files.

---
+++ Hello world/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System;
+
+Console.WriteLine("Madlibs");
+    Console.WriteLine("Enter a name.");
+        string name = Console.ReadLine();
+    Console.WriteLine("Enter a location.");
+        string place = Console.ReadLine();
+    Console.WriteLine("Enter an adjective.");
+        string adjective1 = Console.ReadLine();
+    Console.WriteLine("Enter a name.");
+        string name2 = Console.ReadLine();
+    Console.WriteLine("Enter another name.");
+        string name3 = Console.ReadLine();
+    Console.WriteLine("Enter an adjective.");
+        string adjective2 = Console.ReadLine();
+    Console.WriteLine("Enter an adverb.");
+        string adverb1 = Console.ReadLine();
+    Console.WriteLine("Enter a name.");
+        string name4 = Console.ReadLine();
+    Console.WriteLine("Enter an exclamation.");
+        string exclaimation = Console.ReadLine();
+    Console.WriteLine("Enter a feeling.");
+        string feeling = Console.ReadLine();
+    Console.WriteLine("Enter an adverb.");
+        string adverb2 = Console.ReadLine();
+    Console.WriteLine("Enter an adjective.");
+        string adjective3 = Console.ReadLine();
+
+        Console.WriteLine("Heres what you wrote!");
+
+Console.WriteLine("One day, " + name + " went to " + place + ". He felt very lonely, even though the view was " + adjective1 + ". \r\n    He decided to call his two best friends," + name2 + " and " + name3+". When they came, \r\n    they told " + name + " something had happened. " + name + " went there very " + adverb1 + ". When he came, \r\n    he found out his other friend " + name4 + " was falling off a cliff. " + name + " said, \""+ exclaimation + "!\" \r\n    Inside he was feeling " + adjective2 + ". " + name + " managed to save her. After that, they had a " + adjective3 + " celebration. \r\n    They all laughed.\r\n");
